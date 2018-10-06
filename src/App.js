a1e87631ba42b2ed6a4ec42928db7cd9b24ba7b4
Maksim Golitsyn <maxim@infofree.ru>
2018-10-06 22:26:31 +0500

    Finish content with dynamic control buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import './App.css';
 
@@ -8,31 +8,69 @@ const Logo = styled.div`
 `;
 
 const ControlButton = styled.div`
+  cursor: pointer;
+  ${props => props.active && css`
+    text-shadow: 0px 0px 60px #03FF03
+  `}
 `;
 
-
 const AppLayout = styled.div`
+  padding: 40px;
+`;
+
+const Bar = styled.div`
   display: grid;
   grid-template-columns: 100px auto 100px 100px;
   padding: 40px;
 `;
 
+const Content = styled.div`
+  
+`;
+
 class App extends Component {
+  state = {
+    page: 'dashboard',
+  };
+
+  displayDashboard = () => this.state.page === 'dashboard';
+
+  displaySettings = () => this.state.page === 'settings';
+
+  changeActivePage = (name) => {
+    this.setState({
+      page: name,
+    });
+  };
+
   render() {
+    const {page} = this.state;
+
     return (
       <AppLayout>
-        <Logo>
-          CryptoDash
-        </Logo>
-        <div>
+        <Bar>
+          <Logo>
+            CryptoDash
+          </Logo>
+          <div>
 
-        </div>
-        <ControlButton>
-          Dashboard
-        </ControlButton>
-        <ControlButton>
-          Settings
-        </ControlButton>
+          </div>
+          <ControlButton
+            onClick={() => this.changeActivePage('dashboard')}
+            active={ this.displayDashboard() }
+          >
+            Dashboard
+          </ControlButton>
+          <ControlButton
+            onClick={() => this.changeActivePage('settings')}
+            active={ this.displaySettings() }
+          >
+            Settings
+          </ControlButton>
+          <Content>
+            Content page: { page }
+          </Content>
+        </Bar>
       </AppLayout>
     );
   }
