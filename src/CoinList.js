1fdf3712d1e116767da68292da95890e1c6f0d79
Maksim Golitsyn <maxim@infofree.ru>
2018-10-09 00:09:28 +0500

    Change grid schema in CoinList

--- src/CoinList.js
+++ src/CoinList.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import _ from 'lodash';
 
 import { subtleBoxShadow, lightBlueBackground, greenBoxShadow, redBoxShadow } from './style';
 
-const CoinGrid = styled.div`
+export const CoinGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  ${props =>
+  props.count &&
+  css`
+      grid-template-columns: repeat(${props.count > 5 ? props.count : 5}, 1fr);
+    `} 
   grid-gap: 15px;
   margin-top: 40px;
 `;
@@ -55,7 +59,7 @@ export default function (favorites = false, fetched) {
     : (this.state.filteredCoins && Object.keys(this.state.filteredCoins)) ||
     Object.keys(this.state.coinList).slice(0, 100);
   return (
-    <CoinGrid>
+    <CoinGrid count={favorites && this.state.favorites.length}>
       { coinKeys && coinKeys.map(coinKey => {
         return (
           fetched && <CoinTile
